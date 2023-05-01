59c1873a036cf7a47f4f2f5693b39c4b0bb2c7fd
ori71e2 <ori71e2@gmail.com>
2023-05-01 21:11:25 +0800

    add

--- User/main.c
+++ User/main.c
@@ -4,7 +4,9 @@
 #include "task.h"
 #include "portmacro.h"
 
-static TaskHandle_t led_task_handle = NULL;
+static TaskHandle_t led_task_handle0 = NULL;
+static TaskHandle_t led_task_handle1 = NULL;
+static TaskHandle_t led_task_handle2 = NULL;
 
 void led_init(void)
 {
@@ -12,11 +14,11 @@ void led_init(void)
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2 | GPIO_Pin_4;
 	GPIO_InitStructure.GPIO_Speed =  GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
-void led_task(void *arg)
+void led_task0(void *arg)
 {
 	while(1)                            
 	{
@@ -26,10 +28,32 @@ void led_task(void *arg)
 		vTaskDelay(500/portTICK_PERIOD_MS);
 	}
 }
+void led_task1(void *arg)
+{
+	while(1)                            
+	{
+		GPIO_SetBits(GPIOA, GPIO_Pin_2);
+		vTaskDelay(300/portTICK_PERIOD_MS);
+		GPIO_ResetBits(GPIOA, GPIO_Pin_2);
+		vTaskDelay(300/portTICK_PERIOD_MS);
+	}
+}
+void led_task2(void *arg)
+{
+	while(1)                            
+	{
+		GPIO_SetBits(GPIOA, GPIO_Pin_4);
+		vTaskDelay(100/portTICK_PERIOD_MS);
+		GPIO_ResetBits(GPIOA, GPIO_Pin_4);
+		vTaskDelay(100/portTICK_PERIOD_MS);
+	}
+}
 int main(void) 
 {
 	led_init();
-	xTaskCreate(led_task, "led_task", 1024, NULL, 20, &led_task_handle);
+	xTaskCreate(led_task0, "led_task0", 1024, NULL, 20, &led_task_handle0);
+	xTaskCreate(led_task1, "led_task1", 1024, NULL, 20, &led_task_handle1);
+	xTaskCreate(led_task2, "led_task2", 1024, NULL, 20, &led_task_handle2);
 	vTaskStartScheduler();
 	while(1);
 }
